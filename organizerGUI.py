24113675d9bf3527842992f1fb69a257cf03b487
ioanamoflic <ioanamoflic@yahoo.com>
2019-12-15 04:22:05 +0200

    commit

--- organizerGUI.py
+++ organizerGUI.py
@@ -6,13 +6,40 @@ def Space():
     br = Label(root, text = "", bg = "black")
     br.pack()   
 
+class User_Data:
+    def __init__(self, pwd, temp, umd, wind,mail):
+        self.pwd = pwd
+        self.temp = temp
+        self.umd = umd
+        self.wind = wind 
+        self.mail = mail
+
+def get_dict():
+    users = []
+    db = open('userDB','r')
+    lines = db.readlines()
+    for user in lines:
+        data = user.split(';')
+        users[data[0]] = User_Data(data[1],data[2],data[3],data[4],data[5])
+    return users
+
+dic = get_dict()
+
 def output():
     input = entry.get()
     tempr, umid, vant = weather.predict(input)
+    db = open('outputDB','a+')        
     temp.configure(text = "Temperature: " + str(tempr))
     hum.configure(text = "Humidity: "+ str(umid))
-    press.configure(text = "Atm. pressure: "+str(vant))
-    
+    press.configure(text = "Wind: "+str(vant))
+    stringMare = ""
+    for users in dic:
+        temp0,temp1 = users.temp.split('-')
+        hum0, hum1 = users.umd.split('-')
+        wind0, wind1 = users.wind.split('-')
+        if temp0<=tempr<=temp1 and hum0<=umid<=hum1 and wind0<=vant<=wind1:
+            stringMare+=user+user.mail
+    people.config(text = "People expected to come at event:"+stringMare)
 
 root = Tk()
 root.title("Weather prediction")
@@ -27,8 +54,6 @@ textPredict.pack()
 entry = Entry (root) 
 entry.pack()
 Space()
-
-
 button = Button(root, text = "Get Prediction", command = output, state = ACTIVE)
 button.pack()
 Space()
@@ -38,4 +63,6 @@ hum = Label(root, text = "Humidity: ", bg = "black", fg = "white")
 hum.pack()
 press = Label(root, text = "Pressure: ", bg = "black", fg = "white")
 press.pack()
+people = Label(root, text = "People expected to come at event:", bg = "black", fg = "white")
+people.pack()
 root.mainloop()
